4f5b3dc19092e78647c667f25923284e3669f223
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-06-21 12:31:24 +0800

    onyx: Configure compression_with_xor.mk
    * getprop | grep virtual_ab
    [ro.virtual_ab.batch_writes]: [true]
    [ro.virtual_ab.compression.enabled]: [true]
    [ro.virtual_ab.compression.threads]: [true]
    [ro.virtual_ab.compression.xor.enabled]: [true]
    [ro.virtual_ab.enabled]: [true]
    [ro.virtual_ab.io_uring.enabled]: [true]
    [ro.virtual_ab.userspace.snapshots.enabled]: [true]

--- device.mk
+++ device.mk
@@ -13,8 +13,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Configure core_64_bit_only.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
-# Configure virtual_ab compression.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+# Configure virtual_ab_ota compression_with_xor.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
 
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Fix for CrashRecovery build error
 PRODUCT_APEX_SYSTEM_SERVER_JARS += com.android.crashrecovery:service-crashrecovery
 
-# Configure twrp common.mk
+# Configure twrp config common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # API
