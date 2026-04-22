1e16bd32bc3c9076e35bd388fe9abc35a746d378
UnmoveD <unmoved2121@gmail.com>
2026-04-22 17:34:33 +0000

    onyx: Remove AB_OTA_POSTINSTALL_CONFIG

--- device.mk
+++ device.mk
@@ -7,11 +7,6 @@
 
 LOCAL_PATH := device/xiaomi/onyx
 # A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
