0d032844fb3a519df8e687e3e0b4f0f134cb605c
UnmoveD <unmoved2121@gmail.com>
2026-04-23 22:37:05 +0000

    onyx: Define build-broken bypass configurations

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,8 +7,12 @@
 
 DEVICE_PATH := device/xiaomi/onyx
 
-# For building with minimal manifest
+# Allow for building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_USES_NETWORK := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
 # Architecture
 TARGET_ARCH                := arm64
