8d8d58580042832218424b35df6dfe73b7dca84b
UnmoveD <unmoved2121@gmail.com>
2026-04-23 16:00:11 +0000

    onyx: Define recovery configuration paths

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,15 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4   := true
 TARGET_USERIMAGES_USE_F2FS   := true
 
+TARGET_SYSTEM_PROP := \
+ $(DEVICE_PATH)/system.prop
+
+TARGET_RECOVERY_FSTAB := \
+ $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+
+TARGET_BOARD_INFO_FILE := \
+ $(DEVICE_PATH)/board-info.txt
+
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
