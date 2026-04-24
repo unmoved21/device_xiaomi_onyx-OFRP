e72d90d8bbaf4930d4090eaaaef52fb85fd09d54
UnmoveD <unmoved2121@gmail.com>
2026-04-24 20:19:43 +0000

    onyx: Configure device recovery build flags and optimization settings

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,96 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_onyx-user
-add_lunch_combo twrp_onyx-userdebug
-add_lunch_combo twrp_onyx-eng
+FDEVICE="onyx"
+
+fox_get_target_device() {
+    local chkdev=""
+    
+    if [ -n "$ZSH_VERSION" ]; 
+      then
+        local current_source="${(%):-%x}"
+        chkdev=$(echo "$current_source" | grep -w "$FDEVICE")
+    elif [ -n "$BASH_VERSION" ];
+      then chkdev=$(echo "$BASH_SOURCE" | grep -w "$FDEVICE")
+    fi
+
+    if [ -n "$chkdev" ]; 
+      then FOX_BUILD_DEVICE="$FDEVICE"
+    else
+        if [ -n "$BASH_VERSION" ]; 
+          then chkdev=$(set | grep BASH_ARGV | grep -w "$FDEVICE")
+        elif [ -n "$ZSH_VERSION" ]; 
+          then chkdev=$(echo "$*" | grep -w "$FDEVICE")
+        fi
+        [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+    fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; 
+  then fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ];
+  then
+    export TARGET_DEVICE_ALT="onyx"
+
+    # Binaries & Tools
+    export FOX_USE_BUSYBOX_BINARY=1
+    export FOX_USE_BASH_SHELL=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_SED_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+    export FOX_USE_ZSTD_BINARY=1
+    export FOX_USE_DATE_BINARY=1
+    export FOX_REPLACE_TOOLBOX_GETPROP=1
+
+    # OrangeFox Addons
+    export FOX_ENABLE_APP_MANAGER=1
+    export FOX_DELETE_AROMAFM=1
+    export FOX_DELETE_INITD_ADDON=1
+
+    # KernelSU-Next / SukiSu support
+    export FOX_ENABLE_KERNELSU_NEXT_SUPPORT=1
+    export FOX_ENABLE_SUKISU_SUPPORT=1
+
+    # A/B partitioning
+    export FOX_VIRTUAL_AB_DEVICE=1
+    export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+    export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+
+    # Store settings at /data/recovery instead of internal storage
+    # export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
+
+    # Use latest "magiskboot" binaries as this is a relatively new device
+    export FOX_USE_UPDATED_MAGISKBOOT=1
+
+    # Very interesting, drops the ramdisk image size by ~6MB
+    # While keeping the same boot times. Needs testing.
+    # export FOX_COMPRESS_EXECUTABLES=1
+  
+    # CCACHE
+    export USE_CCACHE=1
+    export CCACHE_EXEC="/usr/bin/ccache"
+    export CCACHE_MAXSIZE="50G"
+    export CCACHE_DIR="/mnt/ccache"
+
+    # Warn if CCACHE_DIR is an invalid directory
+    if [ $USE_CCACHE = 1 ] && [ ! -d ${CCACHE_DIR} ];
+     then
+       echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
+       echo "Please edit the CCACHE_DIR build variable or mount the directory."
+    fi
+
+    export LC_ALL="C"
+    export BUILD_USERNAME=unmoved21
+    export BUILD_HOSTNAME=github
+
+    # Debugging
+    ## export FOX_RESET_SETTINGS=0
+    ## export FOX_INSTALLER_DEBUG_MODE=1
+    ## OF_REPORT_HARMLESS_MOUNT_ISSUES=1
+  else
+    if [ -z "$FOX_BUILD_DEVICE" ] && [ -z "$BASH_SOURCE" ] && [ -z "$ZSH_VERSION" ]; 
+      then echo "I: This script requires bash or zsh. Not processing $FDEVICE"
+    fi
+fi
