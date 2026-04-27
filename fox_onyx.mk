30df9708d3473b4d9476283d71311b8ebf1bcce7
UnmoveD <unmoved2121@gmail.com>
2026-04-27 22:55:23 +0000

    fixup! onyx: Use LZ4 ramdisk compression

--- fox_onyx.mk
+++ fox_onyx.mk
@@ -35,3 +35,6 @@ OF_BIND_MOUNT_SDCARD_ON_FORMAT := 1
 
 # Ignore the loop block errors after flashing ZIPs
 OF_LOOP_DEVICE_ERRORS_TO_LOG := 1
+
+# use lz4 compression (don't duplicate the setting - already set by "BOARD_RAMDISK_USE_LZ4 := true")
+OF_USE_LZ4_COMPRESSION := 1
