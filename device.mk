541d325945fa2fbb6985a68011c8d594ae6bb6f0
UnmoveD <unmoved2121@gmail.com>
2026-04-28 15:05:32 +0000

    onyx: Enable EDL mode support
    Allows rebooting to EDL mode from recovery

--- device.mk
+++ device.mk
@@ -63,6 +63,7 @@ TW_NO_SCREEN_BLANK      := true
 TW_FRAMERATE            := 120
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_HAS_EDL_MODE := true
 
 # Vendor modules required for the recovery to function properly
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
